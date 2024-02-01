93f0ad623d290d057ae1bd0b0bd4ec2486ed7d47
AlessandroTedde <148778362+AlessandroTedde@users.noreply.github.com>
2024-02-01 17:01:46 +0100

    Aggiungere i file di progetto.

---
+++ EsercizioUtenteBED4/MenuPrincipale.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioUtenteBED4
+{
+    public static class MenuPrincipale
+    {
+        public static bool Continue = true;
+        public static void SelezioneMultipla()
+        {
+            while (Continue)
+            {
+                Console.WriteLine("===========OPERAZIONI============");
+                Console.WriteLine("\n Scegli l'operazione da effettuare:");
+                Console.WriteLine("1) Login");
+                Console.WriteLine("2) Logout");
+                Console.WriteLine("3) Controllo data e ora login");
+                Console.WriteLine("4) Storico login");
+                Console.WriteLine("5) Esci");
+                Console.WriteLine("\n");
+
+                string menu = Console.ReadLine();
+                
+                switch (menu)
+                {
+                    case "1":
+                        if (!Utente.User)
+                        {
+                            Console.Clear();
+                            Utente.Login();
+                        } 
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Risulta già un utente loggato. Effettua il logout prima di autenticarti in questa sessione.");
+                            System.Threading.Thread.Sleep(5000);
+                            Console.Clear();
+                        }
+                    break;
+
+                    case "2":
+                        Console.Clear();
+                        Utente.Logout(); 
+                    break;
+
+                    case "3":
+                        Console.Clear();
+                        Utente.DateTimeControl();
+                        break;
+
+                        case "4":
+                            Console.Clear();
+                        Utente.LoginList();
+                        break;
+
+                    case "5":
+                    
+                    default: Continue = false;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ EsercizioUtenteBED4/Utente.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioUtenteBED4
+{
+    public static class Utente
+    {
+        public static bool freeze = true;
+        public static List<string> logins = new List<string>();
+        public static string Username = "";
+        public static string Date = "";
+        public static string Time = "";
+        public static bool User = false;
+        public static void Login()
+        {
+            Console.WriteLine("===========Procedura login============");
+            Console.WriteLine("\n Inserisci il nome utente: ");
+            Username = Console.ReadLine();
+            User = true;
+            Console.WriteLine("Inserisci la password: ");
+            string password = Console.ReadLine();
+            Console.WriteLine("Conferma password: ");
+            string confermaPassword = Console.ReadLine();
+
+            if (User && password == confermaPassword)
+            {
+                Console.WriteLine("Autenticazione effettuata.");
+                Date = DateTime.Now.ToString("dd MMMM yyyy");
+                Time = DateTime.Now.ToString("hh:mm t");
+                logins.Add($"{Username} il {Date} alle {Time}");
+                System.Threading.Thread.Sleep(3000);
+                Console.Clear();
+                MenuPrincipale.Continue = true;
+            } 
+            else
+            {
+                Console.WriteLine("Utente o password non validi.");
+                User = false;
+                System.Threading.Thread.Sleep(3000);
+                Console.Clear();
+                MenuPrincipale.Continue = true;
+            }
+        }
+        public static void Logout()
+        {
+            if (User)
+            {
+                Console.WriteLine("===========Procedura logout============");
+                Console.WriteLine("\n Logout effettuato.");
+                User = false;
+                System.Threading.Thread.Sleep(3000);
+                Console.Clear();
+                MenuPrincipale.Continue = true;
+            }
+            else
+            {
+                Console.WriteLine("\n Impossibile effettuare il logout senza aver prima effettuato il login.");
+                System.Threading.Thread.Sleep(5000);
+                Console.Clear();
+                MenuPrincipale.Continue = true;
+            }
+        }
+
+        public static void DateTimeControl()
+        {
+                Console.WriteLine("===========Controllo data e ora accesso============");
+
+            if (User)
+            {
+                Console.WriteLine($"\n L'utente {Username} ha effettuato il login il {Date} alle {Time}");
+                System.Threading.Thread.Sleep(5000);
+                Console.Clear();
+                MenuPrincipale.Continue = true;
+            } else
+            {
+                Console.WriteLine("\n Non è stato ancora effettuato un login in questa sessione.");
+                System.Threading.Thread.Sleep(4000);
+                Console.Clear();
+                MenuPrincipale.Continue = true;
+            }
+        }
+            public static string[] loginsList = logins.ToArray();
+        public static void LoginList()
+        {
+
+            while (freeze)
+            {
+                Console.Clear();
+                Console.WriteLine("===========Storico login============");
+
+                for (int i = 0; i < loginsList.Length; i++)
+                {
+                    Console.WriteLine($"{loginsList[i]}");
+                }
+                Console.WriteLine("1) Esci");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+
+                    default: freeze = false;
+                        Console.Clear();
+                        break;
+                }
+            }
+        }
+    }
+}
